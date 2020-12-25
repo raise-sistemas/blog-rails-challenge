3654449a233d31c06d31c485f08ba45c55c4d024
Neves <marcos.neves@gmail.com>
2020-12-25 08:54:15 -0300

    add post validations tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
 end
