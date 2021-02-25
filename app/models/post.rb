b1896354a6fa63ce94001b1a2c97f7738d9e3726
raisework <ilha.dev.br@gmail.com>
2021-02-25 13:52:17 -0300

    Remove changes related to other issue (PR)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,3 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
-
-  # Add parameterize for url customization
-  def to_param
-    "#{id}-#{title.parameterize}"
-  end
 end
