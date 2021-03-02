8221b74eb1c70e631e96ae3c4089d1680c6e6962
Andre Costa <mr.luizandre@gmail.com>
2021-03-01 22:33:05 -0300

    Add friendly URL to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+    # gera URL amigável a partir do título
+    # Ex.: "1-primeiro-post"
+  end
 end
