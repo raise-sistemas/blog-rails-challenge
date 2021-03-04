91f7f5eec5b0e7fb6072b4227f5742a2aa006d99
Andre Costa <mr.luizandre@gmail.com>
2021-03-03 21:41:56 -0300

    Add test for model Post pagination

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
+
+  describe 'pagination' do
+    it 'return limited quantity of records based on page' do
+      5.times { Post.create(title: 'Title', body: 'Body')}
+      posts = Post.paginate(page: 1)
+      expect(posts.count).to eq(4)
+      posts = Post.paginate(page: 2)
+      expect(posts.count).to eq(1)
+    end
+  end
 end
