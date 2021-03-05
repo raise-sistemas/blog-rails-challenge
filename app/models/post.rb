09206a9c2d1e5a72a1bbffc112a83044980099ba
Matheus Tsa <matheus.tsa@hotmail.com>
2021-03-05 10:57:10 -0300

    Created to_param method in post.rb to also accept urls in 'id-title-day-id' format, in addition to the standard format

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
+
+  # Replaces special characters in a string so that it may be used as part of a 'pretty' URL.
+  def to_param
+    [id, title.parameterize, created_at.day, id].join('-')
+  end
 end
