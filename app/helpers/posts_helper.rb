c8a275fb7c848c753a0686a99d59b20166f820a1
Matheus Tsa <matheus.tsa@hotmail.com>
2021-03-05 18:19:42 -0300

    Created button_publish_post in posts_helper.rb to be called in view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,11 @@ module PostsHelper
   def link_to_next_post
     @has_next ? link_to( "Next Page", published_posts_path(page: @page + 1)) : "Next Page"
   end
+
+  def button_publish_post
+    # Otherwise created_at attribute is null and application breaks
+    unless @post.new_record?
+      link_to 'Publish', publish_post_path(@post), class: 'button', method: :patch
+    end
+  end
 end
