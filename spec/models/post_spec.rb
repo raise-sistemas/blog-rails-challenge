e85dc91a4388cd74a0070eaed188d449ec40bbea
Matheus Tsa <matheus.tsa@hotmail.com>
2021-03-05 10:16:16 -0300

    Update post_spec.rb with test for post order in published route

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
+
+  describe 'posts order' do
+    it 'should show posts ordered by descending created_at column' do
+      posts = Post.order(created_at: :desc)
+      first_post = posts.first
+      last_post = posts.last
+      expect(last_post.created_at).to > first_post.created_at
+    end
+  end
 end
