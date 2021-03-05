36d1b1ecfff1164e7232db7c7496f341218128d3
Matheus Tsa <matheus.tsa@hotmail.com>
2021-03-05 16:20:41 -0300

    Created new PATCH route to published posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     collection do
       get :published
     end
+
+    member do
+      patch :publish
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
