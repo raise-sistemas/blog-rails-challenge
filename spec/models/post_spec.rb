c108d3b95cca67a0b3d4fa50a2d986c5b1469a47
Jorge <jlimajorge@gmail.com>
2021-04-02 14:54:59 -0300

    test: create Post with correct values to published_at field

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
+
+  context 'create object with correct values' do
+    describe 'published_at' do
+      context 'when it is not defined, it must have the default value' do
+        before do
+          @post = Post.new(title: 'New Title', body: 'New Body')
+          @post.save!
+        end
+
+        it { expect(@post).to be_valid }
+        it { expect(@post.published_at).to be_a Time }
+      end
+    end
+  end
 end
