b1ab86e84f4f8ddef751cf8c0865e5c06bd621d8
Jorge <jlimajorge@gmail.com>
2021-04-02 18:55:10 -0300

    chore: update schema database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_173128) do
+ActiveRecord::Schema.define(version: 2021_04_02_181307) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "published_at", default: "2021-04-01 18:14:41"
+    t.datetime "published_at", default: "2021-04-02 00:00:00"
+    t.integer "status", default: 0
   end
 
 end
