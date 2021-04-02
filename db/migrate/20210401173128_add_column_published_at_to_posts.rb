ed048e1e35a2d2851f2c53fb5398141dfccc27ce
Jorge <jlimajorge@gmail.com>
2021-04-02 11:33:21 -0300

    refactor: change the default value of publised_at to  date current

--- db/migrate/20210401173128_add_column_published_at_to_posts.rb
+++ db/migrate/20210401173128_add_column_published_at_to_posts.rb
@@ -1,5 +1,5 @@
 class AddColumnPublishedAtToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_column :posts, :published_at, :datetime, default: DateTime.current
+    add_column :posts, :published_at, :datetime, default: Date.current
   end
 end
